f3f339807cddd995d69fb2ea67129c743fd3b139
Adrien Tremblay <adrien@adrientremblay.com>
2021-08-26 20:48:20 -0400

    draw to screen buffer

--- snake.cpp
+++ snake.cpp
@@ -1,20 +1,29 @@
-// snake.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <Windows.h>
+using namespace std;
+
+const int screenWidth = 120;
+const int screenHeight = 40;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    wchar_t* screen = new wchar_t[screenWidth * screenHeight];
+    for (int i = 0; i < screenWidth * screenHeight; i++) {
+        screen[i] = '1';
+    }
+
+    cout << screen;
+
+    HANDLE console = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(console);
+    DWORD bytesWritten = 0;
+
+    while (true) 	{
+
+
+        // draw screen to console 
+        screen[screenWidth * screenHeight - 1] = '\0';
+        WriteConsoleOutputCharacter(console, screen, screenWidth * screenHeight, { 0,0 }, &bytesWritten);
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
